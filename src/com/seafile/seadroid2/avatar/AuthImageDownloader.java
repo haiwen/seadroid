9ef8161e54b5271eabaecfee10c4c5bbd5eda46c
logan676 <logan676395@gmail.com>
2014-10-28 18:52:46 +0800

    use cached SSLSocketFactory
    use self keystore trust manager rather than trust all host
    
    reference nostra13/Android-Universal-Image-Loader#240

--- src/com/seafile/seadroid2/avatar/AuthImageDownloader.java
+++ src/com/seafile/seadroid2/avatar/AuthImageDownloader.java
@@ -6,93 +6,65 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Iterator;
+import java.util.Map;
 
-import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
+import javax.net.ssl.SSLSocketFactory;
 
 import android.content.Context;
 import android.util.Log;
 
 import com.nostra13.universalimageloader.core.assist.FlushedInputStream;
 import com.nostra13.universalimageloader.core.download.BaseImageDownloader;
+import com.seafile.seadroid2.SSLTrustManager;
+import com.seafile.seadroid2.account.Account;
+
 
 public class AuthImageDownloader extends BaseImageDownloader {
+    private static final String TAG = AuthImageDownloader.class.getName();
 
-    public static final String DEBUG_TAG = "AuthImageDownloader";
+    public AuthImageDownloader(Context context) {
+        super(context);
+    }
 
-    public AuthImageDownloader(Context context, int connectTimeout, int readTimeout) {
+    public AuthImageDownloader(Context context, int connectTimeout,
+            int readTimeout) {
         super(context, connectTimeout, readTimeout);
     }
 
     @Override
     protected InputStream getStreamFromNetwork(String imageUri, Object extra)
             throws IOException {
-
         URL url = null;
         try {
             url = new URL(imageUri);
         } catch (MalformedURLException e) {
-            Log.e(DEBUG_TAG, e.getMessage(), e);
+            Log.e(TAG, e.getMessage(), e);
         }
+
         HttpURLConnection http = null;
 
         if (Scheme.ofUri(imageUri) == Scheme.HTTPS) {
-            trustAllHosts();
+           Map<Account, SSLSocketFactory> cachedFactories = SSLTrustManager.instance().getCachedFactories();
+           Iterator it = cachedFactories.entrySet().iterator();
+            while (it.hasNext()) {
+                Map.Entry pairs = (Map.Entry) it.next();
+                HttpsURLConnection
+                        .setDefaultSSLSocketFactory((SSLSocketFactory) pairs
+                                .getValue());
+                it.remove(); // avoids a ConcurrentModificationException
+            }
+            /*HttpsURLConnection
+                    .setDefaultHostnameVerifier(MyCustomSSLSocketHandler.getCustomHostnameVerifier());*/
             HttpsURLConnection https = (HttpsURLConnection) url.openConnection();
-            https.setHostnameVerifier(DO_NOT_VERIFY);
             http = https;
             http.connect();
         } else {
             http = (HttpURLConnection) url.openConnection();
         }
-
         http.setConnectTimeout(connectTimeout);
         http.setReadTimeout(readTimeout);
         return new FlushedInputStream(new BufferedInputStream(http.getInputStream()));
-    }
-
-    // always verify the host - dont check for certificate
-    final static HostnameVerifier DO_NOT_VERIFY = new HostnameVerifier() {
-        @Override
-        public boolean verify(String hostname, SSLSession session) {
-            return true;
-        }
-    };
-
-    /**
-     * Trust every server - dont check for any certificate
-     */
-    private static void trustAllHosts() {
-        // Create a trust manager that does not validate certificate chains
-        TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
-            @Override
-            public void checkClientTrusted(java.security.cert.X509Certificate[] x509Certificates, String s) 
-                    throws java.security.cert.CertificateException {
-            }
-
-            @Override
-            public void checkServerTrusted(java.security.cert.X509Certificate[] x509Certificates, String s) 
-                    throws java.security.cert.CertificateException {
-            }
-
-            @Override
-            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                return new java.security.cert.X509Certificate[] {};
-            }
-        } };
-
-        // Install the all-trusting trust manager
-        try {
-            SSLContext sc = SSLContext.getInstance("TLS");
-            sc.init(null, trustAllCerts, new java.security.SecureRandom());
-            HttpsURLConnection
-                    .setDefaultSSLSocketFactory(sc.getSocketFactory());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
