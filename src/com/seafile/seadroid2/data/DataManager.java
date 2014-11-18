43780a5610c56e15c06860210edcfc9a8ca8146b
Logan <logan676395@gmail.com>
2014-11-18 17:44:22 +0800

    fix incorrect imports

--- src/com/seafile/seadroid2/data/DataManager.java
+++ src/com/seafile/seadroid2/data/DataManager.java
@@ -5,7 +5,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
