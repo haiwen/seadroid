dde21c841853855fff29b635c0d94fe7d92358ed
Logan <logan676395@gmail.com>
2015-07-22 14:35:57 +0800

    redraw the list only when data set changed

--- src/com/seafile/seadroid2/ui/adapter/SeafItemAdapter.java
+++ src/com/seafile/seadroid2/ui/adapter/SeafItemAdapter.java
@@ -64,19 +64,31 @@ public class SeafItemAdapter extends BaseAdapter {
      * @param newList
      */
     public void setDownloadTaskList(List<DownloadTaskInfo> newList) {
-
-        if (this.mDownloadTaskInfos == null || newList.size() != this.mDownloadTaskInfos.size()) {
+        if (!equalLists(newList, mDownloadTaskInfos)) {
             this.mDownloadTaskInfos = newList;
+            // redraw the list
             notifyDataSetChanged();
-            return;
         }
-        for (int i = 0; i < newList.size(); i++) {
-            if (!newList.get(i).equals(this.mDownloadTaskInfos.get(i))) {
-                break;
-            }
-        }
-        this.mDownloadTaskInfos = newList;
-        notifyDataSetChanged();
+    }
+
+    /**
+     * Compare two lists
+     *
+     * @param newList
+     * @param oldList
+     * @return true if the two lists are equal,
+     *         false, otherwise.
+     */
+    private boolean equalLists(List<DownloadTaskInfo> newList, List<DownloadTaskInfo> oldList) {
+        if (newList == null && oldList == null)
+            return true;
+
+        if ((newList == null && oldList != null)
+                || newList != null && oldList == null
+                || newList.size() != oldList.size())
+            return false;
+
+        return newList.equals(oldList);
     }
 
     public void addEntry(SeafItem entry) {
