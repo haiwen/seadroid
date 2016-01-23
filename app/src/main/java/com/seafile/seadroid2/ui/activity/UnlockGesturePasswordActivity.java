74625347962e40f551f6d13897f5f31ad5b72667
Logan <logan676395@gmail.com>
2016-01-23 10:38:39 +0800

    fix after upgrading to seadroid2, crashes on start #474
    fix #474

--- app/src/main/java/com/seafile/seadroid2/ui/activity/UnlockGesturePasswordActivity.java
+++ app/src/main/java/com/seafile/seadroid2/ui/activity/UnlockGesturePasswordActivity.java
@@ -39,8 +39,8 @@ public class UnlockGesturePasswordActivity extends BaseActivity implements Toolb
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.gesturepassword_unlock);
