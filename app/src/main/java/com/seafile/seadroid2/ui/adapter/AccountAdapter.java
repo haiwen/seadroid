haiwen/seadroid

commit bdb35829286d663140d5daaa94fee9b02cd52a0f

Use circular ImageView for profile images

app/src/main/java/com/seafile/seadroid2/ui/adapter/AccountAdapter.java
@@ -18,6 +18,7 @@ import android.widget.TextView;
 import com.google.common.collect.Lists;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
 import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
 import com.nostra13.universalimageloader.core.display.RoundedBitmapDisplayer;
 import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
@@ -25,6 +26,7 @@ import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListene
 import com.seafile.seadroid2.R;
 import com.seafile.seadroid2.account.Account;
 import com.seafile.seadroid2.avatar.Avatar;
+import com.seafile.seadroid2.ui.widget.CircleImageView;
 
 /**
  * Base account adapter
@@ -32,8 +34,6 @@ import com.seafile.seadroid2.avatar.Avatar;
 public abstract class AccountAdapter extends BaseAdapter {
     private static final String DEBUG_TAG = "AccountAdapter";
     
-    private ImageLoadingListener animateFirstListener = new AnimateFirstDisplayListener();
-    private DisplayImageOptions options;
     private ArrayList<Account> items;
     private ArrayList<Avatar> avatars;
     private Context context;
@@ -54,11 +54,6 @@ public abstract class AccountAdapter extends BaseAdapter {
         return items.isEmpty();
     }
 
-    public void addEntry(Account entry) {
-        items.add(entry);
-        notifyDataSetChanged();
-    }
-
     public void add(Account entry) {
         items.add(entry);
     }
@@ -70,11 +65,6 @@ public abstract class AccountAdapter extends BaseAdapter {
     @Override
     public Account getItem(int position) {
         return items.get(position);
-    }
-
-    public void setItem(Account item, int listviewPosition) {
-        items.set(listviewPosition, item);
-        notifyDataSetChanged();
     }
 
     public void setItems(List<Account> items) {
@@ -113,7 +103,7 @@ public abstract class AccountAdapter extends BaseAdapter {
             view = LayoutInflater.from(context).inflate(getChildLayout(), null);
             TextView title = (TextView) view.findViewById(getChildTitleId());
             TextView subtitle = (TextView) view.findViewById(getChildSubTitleId());
-            ImageView icon = (ImageView) view.findViewById(getChildIconId());
+            CircleImageView icon = (CircleImageView) view.findViewById(getChildIconId());
             viewHolder = new Viewholder(title, subtitle, icon);
             view.setTag(viewHolder);
         } else {
@@ -123,20 +113,7 @@ public abstract class AccountAdapter extends BaseAdapter {
         viewHolder.title.setText(account.getServerHost());
         viewHolder.subtitle.setText(account.getEmail());
         if (getAvatarUrl(account) != null) {
-            options = new DisplayImageOptions.Builder()
-            .extraForDownloader(account)
-            .showStubImage(R.drawable.default_avatar)
-            // .delayBeforeLoading(1000)
-            .showImageOnLoading(R.drawable.default_avatar)
-            .showImageForEmptyUri(R.drawable.default_avatar)
-            .showImageOnFail(R.drawable.default_avatar)
-            .resetViewBeforeLoading()
-            .cacheInMemory(true)
-            .cacheOnDisk(true)
-            .considerExifParams(true)
-            .displayer(new RoundedBitmapDisplayer(50))
-            .build();
-            ImageLoader.getInstance().displayImage(getAvatarUrl(account), viewHolder.icon, options, animateFirstListener);
+            ImageLoader.getInstance().displayImage(getAvatarUrl(account), viewHolder.icon);
         }
         ImageLoader.getInstance().handleSlowNetwork(true);
         
@@ -156,28 +133,11 @@ public abstract class AccountAdapter extends BaseAdapter {
         return null;
     }
 
-    private static class AnimateFirstDisplayListener extends SimpleImageLoadingListener {
-
-        static final List<String> displayedImages = Collections.synchronizedList(new LinkedList<String>());
-
-        @Override
-        public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
-            if (loadedImage != null) {
-                ImageView imageView = (ImageView) view;
-                boolean firstDisplay = !displayedImages.contains(imageUri);
-                if (firstDisplay) {
-                    FadeInBitmapDisplayer.animate(imageView, 500);
-                    displayedImages.add(imageUri);
-                }
-            }
-        }
-    }
-    
     private class Viewholder {
         TextView title, subtitle;
-        ImageView icon;
+        CircleImageView icon;
 
-        public Viewholder(TextView title, TextView subtitle, ImageView icon) {
+        public Viewholder(TextView title, TextView subtitle, CircleImageView icon) {
             super();
             this.icon = icon;
             this.title = title;

app/src/main/res/layout-sw320dp/list_item_account_entry.xml
@@ -7,8 +7,8 @@
               android:background="?android:selectableItemBackground"
               android:padding="0dp">
 
-    <ImageView
-            android:id="@+id/list_item_account_icon"
+    <com.seafile.seadroid2.ui.widget.CircleImageView
+        android:id="@+id/list_item_account_icon"
             android:layout_width="@dimen/lv_act_icon_width"
             android:layout_height="@dimen/lv_act_icon_height"
             android:layout_marginTop="@dimen/lv_act_icon_margin_top"

app/src/main/res/layout-sw600dp/list_item_account_entry.xml
@@ -7,7 +7,7 @@
               android:gravity="center_horizontal"
               android:padding="0dp">
 
-    <ImageView
+    <com.seafile.seadroid2.ui.widget.CircleImageView
             android:id="@+id/list_item_account_icon"
             android:layout_width="@dimen/lv_act_icon_width"
             android:layout_height="@dimen/lv_act_icon_height"

app/src/main/res/layout-xlarge/list_item_account_entry.xml
@@ -7,8 +7,8 @@
 			  android:gravity="center_horizontal"
 			  android:padding="0dp">
 
-	<ImageView
-			android:id="@+id/list_item_account_icon"
+	<com.seafile.seadroid2.ui.widget.CircleImageView
+		android:id="@+id/list_item_account_icon"
 			android:layout_width="@dimen/lv_act_icon_width"
 			android:layout_height="@dimen/lv_act_icon_height"
 			android:layout_marginTop="@dimen/lv_act_icon_margin_top"

app/src/main/res/layout/cuc_account_list_item.xml
@@ -3,9 +3,10 @@
               android:layout_width="match_parent"
               android:layout_height="wrap_content"
               android:background="?android:selectableItemBackground"
+              android:gravity="center_vertical"
               android:padding="@dimen/lv_item_padding" >
 
-    <ImageView
+    <com.seafile.seadroid2.ui.widget.CircleImageView
             android:id="@+id/cuc_account_list_item_account_icon"
             android:layout_width="@dimen/lv_act_icon_width"
             android:layout_height="@dimen/lv_act_icon_height"

app/src/main/res/layout/list_item_account_entry.xml
@@ -7,7 +7,7 @@
               android:background="?android:selectableItemBackground"
               android:padding="0dp">
 
-    <ImageView
+    <com.seafile.seadroid2.ui.widget.CircleImageView
             android:id="@+id/list_item_account_icon"
             android:layout_width="@dimen/lv_act_icon_width"
             android:layout_height="@dimen/lv_act_icon_height"

commit 4d1b2244bfb3ba1f64cd376e71813bc05fa056a0

Fix Account.token NPE when loading avatar by AuthImageDownloader getStreamFromNetwork
The bug introduced by commit bdb35829286d663140d5daaa94fee9b02cd52a0f

app/src/main/java/com/seafile/seadroid2/ui/adapter/AccountAdapter.java
@@ -33,7 +33,9 @@ import com.seafile.seadroid2.ui.widget.CircleImageView;
  */
 public abstract class AccountAdapter extends BaseAdapter {
     private static final String DEBUG_TAG = "AccountAdapter";
-    
+
+    private ImageLoadingListener animateFirstListener = new AnimateFirstDisplayListener();
+    private DisplayImageOptions options;
     private ArrayList<Account> items;
     private ArrayList<Avatar> avatars;
     private Context context;
@@ -113,7 +115,20 @@ public abstract class AccountAdapter extends BaseAdapter {
         viewHolder.title.setText(account.getServerHost());
         viewHolder.subtitle.setText(account.getEmail());
         if (getAvatarUrl(account) != null) {
-            ImageLoader.getInstance().displayImage(getAvatarUrl(account), viewHolder.icon);
+            options = new DisplayImageOptions.Builder()
+                    .extraForDownloader(account)
+                    .showStubImage(R.drawable.default_avatar)
+                    // .delayBeforeLoading(1000)
+                    .showImageOnLoading(R.drawable.default_avatar)
+                    .showImageForEmptyUri(R.drawable.default_avatar)
+                    .showImageOnFail(R.drawable.default_avatar)
+                    .resetViewBeforeLoading()
+                    .cacheInMemory(true)
+                    .cacheOnDisk(true)
+                    .considerExifParams(true)
+                    .displayer(new RoundedBitmapDisplayer(50))
+                    .build();
+            ImageLoader.getInstance().displayImage(getAvatarUrl(account), viewHolder.icon, options, animateFirstListener);
         }
         ImageLoader.getInstance().handleSlowNetwork(true);
         
@@ -131,6 +146,23 @@ public abstract class AccountAdapter extends BaseAdapter {
         }
 
         return null;
+    }
+
+    private static class AnimateFirstDisplayListener extends SimpleImageLoadingListener {
+
+        static final List<String> displayedImages = Collections.synchronizedList(new LinkedList<String>());
+
+        @Override
+        public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+            if (loadedImage != null) {
+                ImageView imageView = (ImageView) view;
+                boolean firstDisplay = !displayedImages.contains(imageUri);
+                if (firstDisplay) {
+                    FadeInBitmapDisplayer.animate(imageView, 500);
+                    displayedImages.add(imageUri);
+                }
+            }
+        }
     }
 
     private class Viewholder {