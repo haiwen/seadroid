haiwen/seadroid

commit a4d2a026e11fe53e0106b1d799aee7330a2d5d5b

Support remote wipe

app/src/main/java/com/seafile/seadroid2/SeafConnection.java
@@ -278,8 +278,7 @@ public class SeafConnection {
             HttpRequest req = prepareApiGetRequest(apiPath, params);
             checkRequestResponseStatus(req, HttpURLConnection.HTTP_OK);
 
-            String result = new String(req.bytes(), "UTF-8");
-            return result;
+            return new String(req.bytes(), "UTF-8");
         } catch (SeafException e) {
             throw e;
         } catch (HttpRequestException e) {
@@ -314,8 +313,7 @@ public class SeafConnection {
             HttpRequest req = prepareApiGetRequest("api2/starredfiles/");
             checkRequestResponseStatus(req, HttpURLConnection.HTTP_OK);
 
-            String result = new String(req.bytes(), "UTF-8");
-            return result;
+            return new String(req.bytes(), "UTF-8");
         } catch (SeafException e) {
             throw e;
         } catch (HttpRequestException e) {
@@ -1280,6 +1278,18 @@ public class SeafConnection {
         }
     }
 
+    public void completeRemoteWipe(String token) throws SeafException {
+        try {
+            HttpRequest req = prepareApiPostRequest("api2/device-wiped/", true, null);
+            req.form("token", token);
+            checkRequestResponseStatus(req, HttpURLConnection.HTTP_CREATED);
+        } catch (SeafException e) {
+            throw e;
+        } catch (HttpRequestException e) {
+            throw getSeafExceptionFromHttpRequestException(e);
+        }
+    }
+
     public void star(String repoID, String path) throws SeafException {
         try {
             HttpRequest req = prepareApiPostRequest("api2/starredfiles/", true, null);
@@ -1491,11 +1501,15 @@ public class SeafConnection {
 
             if (req.message() == null) {
                 throw SeafException.networkException;
+            } else if (req.code() == HttpURLConnection.HTTP_UNAUTHORIZED) {
+                String wiped = req.header("X-Seafile-Wiped");
+                if (wiped != null) {
+                    throw SeafException.remoteWipedException;
+                }
             } else {
                 throw new SeafException(req.code(), req.message());
             }
-        }
-        else {
+        } else {
             // Log.v(DEBUG_TAG, "HTTP request ok : " + req.url());
         }
     }

app/src/main/java/com/seafile/seadroid2/SeafException.java
@@ -20,6 +20,7 @@ public class SeafException extends Exception {
     public static final SeafException blockListNullPointerException = new SeafException(9, "block list is null");
     public static final SeafException encryptException = new SeafException(10, "encryption key or iv is null");
     public static final SeafException decryptException = new SeafException(11, "decryption key or iv is null");
+    public static final SeafException remoteWipedException = new SeafException(12, "Remote Wiped Error");
 
     public SeafException(int code, String msg) {
         super(msg);

app/src/main/java/com/seafile/seadroid2/data/DataManager.java
@@ -838,6 +838,10 @@ public class DataManager {
         passwords.clear();
     }
 
+    public void completeRemoteWipe() throws SeafException {
+        sc.completeRemoteWipe(account.token);
+    }
+
     private static class PasswordInfo {
         String password;
         long timestamp;

app/src/main/java/com/seafile/seadroid2/ui/activity/BrowserActivity.java
@@ -48,10 +48,12 @@ import com.seafile.seadroid2.account.Account;
 import com.seafile.seadroid2.account.AccountManager;
 import com.seafile.seadroid2.cameraupload.MediaObserverService;
 import com.seafile.seadroid2.data.DataManager;
+import com.seafile.seadroid2.data.DatabaseHelper;
 import com.seafile.seadroid2.data.SeafDirent;
 import com.seafile.seadroid2.data.SeafRepo;
 import com.seafile.seadroid2.data.SeafStarredFile;
 import com.seafile.seadroid2.data.ServerInfo;
+import com.seafile.seadroid2.data.StorageManager;
 import com.seafile.seadroid2.fileschooser.MultiFileChooserActivity;
 import com.seafile.seadroid2.monitor.FileMonitorService;
 import com.seafile.seadroid2.notification.DownloadNotificationProvider;
@@ -406,6 +408,54 @@ public class BrowserActivity extends BaseActivity
             return;
 
         ConcurrentAsyncTask.execute(new RequestServerInfoTask());
+    }
+
+    public void completeRemoteWipe() {
+        ConcurrentAsyncTask.execute(new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... objects) {
+                // clear local caches
+                StorageManager storageManager = StorageManager.getInstance();
+                storageManager.clearCache();
+
+                // clear cached data from database
+                DatabaseHelper dbHelper = DatabaseHelper.getDatabaseHelper();
+                dbHelper.delCaches();
+
+                try {
+                    // response to server when finished cleaning caches
+                    getDataManager().completeRemoteWipe();
+                } catch (SeafException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void o) {
+                // sign out current account
+                logoutWhenTokenExpired();
+
+            }
+        });
+    }
+
+    /**
+     * Token expired, clear current authorized info and redirect user to login page
+     */
+    public void logoutWhenTokenExpired() {
+        AccountManager accountMgr = new AccountManager(this);
+
+        // sign out current account
+        Account account = accountMgr.getCurrentAccount();
+        accountMgr.signOutAccount(account);
+
+        // then redirect to AccountsActivity
+        Intent intent = new Intent(this, AccountsActivity.class);
+        startActivity(intent);
+
+        // finish current Activity
+        finish();
     }
 
     @Override

app/src/main/java/com/seafile/seadroid2/ui/fragment/ActivitiesFragment.java
@@ -389,8 +389,12 @@ public class ActivitiesFragment extends Fragment {
 
             if (result == null) {
                 if (err != null) {
-                    ToastUtils.show(mActivity, err.getMessage());
-                    showError(R.string.error_when_load_activities);
+                    if (err == SeafException.remoteWipedException) {
+                        mActivity.completeRemoteWipe();
+                    } else {
+                        ToastUtils.show(mActivity, err.getMessage());
+                        showError(R.string.error_when_load_activities);
+                    }
                 }
                 return;
             }

app/src/main/java/com/seafile/seadroid2/ui/fragment/ReposFragment.java
@@ -31,11 +31,13 @@ import com.seafile.seadroid2.SettingsManager;
 import com.seafile.seadroid2.account.Account;
 import com.seafile.seadroid2.account.AccountManager;
 import com.seafile.seadroid2.data.DataManager;
+import com.seafile.seadroid2.data.DatabaseHelper;
 import com.seafile.seadroid2.data.SeafCachedFile;
 import com.seafile.seadroid2.data.SeafDirent;
 import com.seafile.seadroid2.data.SeafGroup;
 import com.seafile.seadroid2.data.SeafItem;
 import com.seafile.seadroid2.data.SeafRepo;
+import com.seafile.seadroid2.data.StorageManager;
 import com.seafile.seadroid2.ssl.CertsManager;
 import com.seafile.seadroid2.transfer.TransferService;
 import com.seafile.seadroid2.ui.CopyMoveContext;
@@ -761,13 +763,15 @@ public class ReposFragment extends ListFragment {
                 });
                 dialog.show(getFragmentManager(), SslConfirmDialog.FRAGMENT_TAG);
                 return;
+            } else if (err == SeafException.remoteWipedException) {
+                mActivity.completeRemoteWipe();
             }
 
             if (err != null) {
                 if (err.getCode() == HttpURLConnection.HTTP_UNAUTHORIZED) {
                     // Token expired, should login again
                     ToastUtils.show(mActivity, R.string.err_token_expired);
-                    logoutWhenTokenExpired();
+                    mActivity.logoutWhenTokenExpired();
                 } else {
                     Log.e(DEBUG_TAG, "failed to load repos: " + err.getMessage());
                     showError(R.string.error_when_load_repos);
@@ -933,6 +937,8 @@ public class ReposFragment extends ListFragment {
                 });
                 dialog.show(getFragmentManager(), SslConfirmDialog.FRAGMENT_TAG);
                 return;
+            } else if (err == SeafException.remoteWipedException) {
+                mActivity.completeRemoteWipe();
             }
 
             if (err != null) {
@@ -941,7 +947,7 @@ public class ReposFragment extends ListFragment {
                 } else if (err.getCode() == HttpURLConnection.HTTP_UNAUTHORIZED) {
                     // Token expired, should login again
                     ToastUtils.show(mActivity, R.string.err_token_expired);
-                    logoutWhenTokenExpired();
+                    mActivity.logoutWhenTokenExpired();
                 } else if (err.getCode() == HttpURLConnection.HTTP_NOT_FOUND) {
                     ToastUtils.show(mActivity, String.format("The folder \"%s\" was deleted", myPath));
                 } else {
@@ -960,24 +966,6 @@ public class ReposFragment extends ListFragment {
             getDataManager().setDirsRefreshTimeStamp(myRepoID, myPath);
             updateAdapterWithDirents(dirents);
         }
-    }
-
-    /**
-     * Token expired, clear current authorized info and redirect user to login page
-     */
-    private void logoutWhenTokenExpired() {
-        AccountManager accountMgr = new AccountManager(mActivity);
-
-        // sign out current account
-        Account account = accountMgr.getCurrentAccount();
-        accountMgr.signOutAccount(account);
-
-        // then redirect to AccountsActivity
-        Intent intent = new Intent(mActivity, AccountsActivity.class);
-        mActivity.startActivity(intent);
-
-        // finish current Activity
-        mActivity.finish();
     }
 
     private void showPasswordDialog() {

app/src/main/java/com/seafile/seadroid2/ui/fragment/StarredFragment.java
@@ -303,8 +303,12 @@ public class StarredFragment extends ListFragment {
 
 
             if (err != null) {
-                showError(getString(R.string.error_when_load_starred));
-                return;
+                if (err == SeafException.remoteWipedException) {
+                    mActivity.completeRemoteWipe();
+                } else {
+                    showError(getString(R.string.error_when_load_starred));
+                    return;
+                }
             }
 
             if (starredFiles == null) {

app/src/main/res/values/strings.xml
@@ -188,6 +188,7 @@
     <string name="seconds_ago">%d seconds ago</string>
     <string name="just_now">Just now</string>
     <string name="error_when_load_starred">Error when loading starred files</string>
+    <string name="error_when_load_events">Error when loading activities</string>
     <string name="open_as">Open as</string>
     <string name="select_upload_files">Select files to upload</string>
     <string name="storage_removed">Storage was removed</string>

commit 10787b2ba3ffbd58302f36ed1f830ca77166c24b

Make detection of invalidated token work again
Fix regression introduced in a4d2a026.

app/src/main/java/com/seafile/seadroid2/SeafConnection.java
@@ -1557,6 +1557,8 @@ public class SeafConnection {
                 String wiped = req.header("X-Seafile-Wiped");
                 if (wiped != null) {
                     throw SeafException.remoteWipedException;
+                } else {
+                    throw new SeafException(req.code(), req.message());
                 }
             } else {
                 throw new SeafException(req.code(), req.message());