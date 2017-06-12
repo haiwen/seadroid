4fd6f41a951dd0b9d0ccae819c5731a2ce6a94e1
maxiaohong <submit2mxh@gmail.com>
2017-06-12 18:55:20 +0800

    update targetSDKVersion for Android 7.0 , fix app in version 25 crash

--- app/src/main/java/com/seafile/seadroid2/ui/activity/BrowserActivity.java
+++ app/src/main/java/com/seafile/seadroid2/ui/activity/BrowserActivity.java
@@ -26,6 +26,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
@@ -1144,8 +1145,12 @@ public class BrowserActivity extends BaseActivity
 
             String fileName = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()) + ".jpg";
             takeCameraPhotoTempFile = new File(ImgDir, fileName);
-
-            Uri photo = Uri.fromFile(takeCameraPhotoTempFile);
+            Uri photo = null;
+            if (android.os.Build.VERSION.SDK_INT > 23) {
+                photo = FileProvider.getUriForFile(this, getApplicationContext().getPackageName() + ".provider", takeCameraPhotoTempFile);
+            } else {
+                photo = Uri.fromFile(takeCameraPhotoTempFile);
+            }
             imageCaptureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photo);
             startActivityForResult(imageCaptureIntent, TAKE_PHOTO_REQUEST);
 
@@ -1804,8 +1809,12 @@ public class BrowserActivity extends BaseActivity
 
     private void chooseExportApp(final String repoName, final String repoID, final String path, final long fileSize) {
         final File file = dataManager.getLocalRepoFile(repoName, repoID, path);
-        Uri uri = Uri.fromFile(file);
-
+        Uri uri = null;
+        if (android.os.Build.VERSION.SDK_INT > 23) {
+            uri = FileProvider.getUriForFile(this, getApplicationContext().getPackageName() + ".provider", file);
+        } else {
+            uri = Uri.fromFile(file);
+        }
         final Intent sendIntent = new Intent();
         sendIntent.setAction(Intent.ACTION_SEND);
         sendIntent.setType(Utils.getFileMimeType(file));
