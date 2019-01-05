742792ebe5389d163e699a2a6409c2c5385d5895
maxiaohong <submit2mxh@gmail.com>
2019-01-05 15:36:57 +0800

    fix IllegalStateException in Android 8 should user startForegroundService api

--- app/src/main/java/com/seafile/seadroid2/BootAutostart.java
+++ app/src/main/java/com/seafile/seadroid2/BootAutostart.java
@@ -3,6 +3,7 @@ package com.seafile.seadroid2;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.util.Log;
 
 import com.seafile.seadroid2.cameraupload.MediaObserverService;
@@ -10,7 +11,6 @@ import com.seafile.seadroid2.cameraupload.MediaObserverService;
 /**
  * This receiver is called whenever the system has booted or
  * the Seadroid app has been upgraded to a new version.
- *
  * It can be used to start up background services.
  */
 public class BootAutostart extends BroadcastReceiver {
@@ -18,15 +18,18 @@ public class BootAutostart extends BroadcastReceiver {
 
     /**
      * This method will be excecuted after
-     *  - booting the device
-     *  - upgrade of the Seadroid package
+     * - booting the device
+     * - upgrade of the Seadroid package
      */
-    public void onReceive(Context context, Intent intent)
-    {
+    public void onReceive(Context context, Intent intent) {
         Log.i(DEBUG_TAG, "Registering for MediaProvider changes.");
 
         Intent mediaObserver = new Intent(context, MediaObserverService.class);
-        context.startService(mediaObserver);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(mediaObserver);
+        } else {
+            context.startService(mediaObserver);
+        }
     }
 
 }
