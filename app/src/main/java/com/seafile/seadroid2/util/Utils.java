c10a566136c1bc4cf690cfb69c97c22f793687b1
maxiaoping <634115572@qq.com>
2021-01-17 12:28:57 +0800

    update log

--- app/src/main/java/com/seafile/seadroid2/util/Utils.java
+++ app/src/main/java/com/seafile/seadroid2/util/Utils.java
@@ -956,14 +956,23 @@ public class Utils {
         return "";
     }
 
-    public static String getRealPathFromURI(Context context, Uri contentUri) {
+    public static String getRealPathFromURI(Context context, Uri contentUri, String mediaId) {
         Cursor cursor = null;
         try {
-            String[] proj = {MediaStore.Images.Media.DATA};
-            cursor = context.getContentResolver().query(contentUri, proj, null, null, null);
-            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-            cursor.moveToFirst();
-            return cursor.getString(column_index);
+            if (MediaStore.Images.Media._ID.equals(mediaId)) {//image
+                String[] proj = {MediaStore.Images.Media.DATA};
+                cursor = context.getContentResolver().query(contentUri, proj, null, null, null);
+                int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                cursor.moveToFirst();
+                return cursor.getString(column_index);
+            } else {//Video
+                String[] proj = {MediaStore.Video.Media.DATA};
+                cursor = context.getContentResolver().query(contentUri, proj, null, null, null);
+                int column_index = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DATA);
+                cursor.moveToFirst();
+                return cursor.getString(column_index);
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
             return null;
