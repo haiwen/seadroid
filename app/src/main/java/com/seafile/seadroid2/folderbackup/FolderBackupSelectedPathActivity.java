9dedefa13e82511f5385761aca2e5805e098acc6
zhwanng <48609908+zhwanng@users.noreply.github.com>
2023-04-07 14:46:23 +0800

    update FolderBackupSelectedPath feature
    - upgrade gradle version

--- app/src/main/java/com/seafile/seadroid2/folderbackup/FolderBackupSelectedPathActivity.java
+++ app/src/main/java/com/seafile/seadroid2/folderbackup/FolderBackupSelectedPathActivity.java
@@ -4,7 +4,6 @@ import static com.seafile.seadroid2.folderbackup.FolderBackupConfigActivity.BACK
 import static com.seafile.seadroid2.folderbackup.FolderBackupConfigActivity.BACKUP_SELECT_PATHS_SWITCH;
 import static com.seafile.seadroid2.ui.fragment.SettingsFragment.FOLDER_BACKUP_REMOTE_PATH;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -16,25 +15,20 @@ import android.view.MenuItem;
 import android.view.View;
 
 import com.blankj.utilcode.util.CollectionUtils;
-import com.cocosw.bottomsheet.BottomSheet;
 import com.seafile.seadroid2.R;
 import com.seafile.seadroid2.SettingsManager;
-import com.seafile.seadroid2.data.EventDetailsFileItem;
-import com.seafile.seadroid2.data.SearchedFile;
 import com.seafile.seadroid2.folderbackup.selectfolder.StringTools;
 import com.seafile.seadroid2.listener.OnItemClickListener;
 import com.seafile.seadroid2.ui.activity.BaseActivity;
-import com.seafile.seadroid2.ui.adapter.BottomSheetAdapter;
-import com.seafile.seadroid2.ui.bottomsheet.BottomSheetListFragment;
 import com.seafile.seadroid2.ui.bottomsheet.BottomSheetTextFragment;
 import com.seafile.seadroid2.ui.widget.SupportRecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class FolderBackupSelectedPathActivity extends BaseActivity implements Toolbar.OnMenuItemClickListener {
+public class FolderBackupSelectedPathActivity extends BaseActivity {
     private SupportRecyclerView mRecyclerView;
-    private FolderBackSelectedPatRecyclerViewAdapter mAdapter;
+    private FolderBackSelectedPathRecyclerViewAdapter mAdapter;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,11 +36,20 @@ public class FolderBackupSelectedPathActivity extends BaseActivity implements To
         setContentView(R.layout.folder_backup_selected_path_activity);
 
         mRecyclerView = findViewById(R.id.lv_search);
-        mAdapter = new FolderBackSelectedPatRecyclerViewAdapter(this);
+        mAdapter = new FolderBackSelectedPathRecyclerViewAdapter(this);
         mAdapter.setOnItemClickListener(new OnItemClickListener<String>() {
             @Override
             public void onItemClick(String text, int position) {
                 showBottomDialog(text);
+            }
+        });
+
+        findViewById(R.id.add_backup_folder).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FolderBackupSelectedPathActivity.this, FolderBackupConfigActivity.class);
+                intent.putExtra(FOLDER_BACKUP_REMOTE_PATH, true);
+                startActivity(intent);
             }
         });
 
@@ -83,13 +86,6 @@ public class FolderBackupSelectedPathActivity extends BaseActivity implements To
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getActionBarToolbar().inflateMenu(R.menu.menu_add_delete);
-        getActionBarToolbar().setOnMenuItemClickListener(this);
-        return true;
-    }
-
-    @Override
     public void onBackPressed() {
         setFinishPage();
 
@@ -114,21 +110,5 @@ public class FolderBackupSelectedPathActivity extends BaseActivity implements To
             intent.putExtra(BACKUP_SELECT_PATHS_SWITCH, true);
         }
         setResult(RESULT_OK, intent);
-    }
-
-    @Override
-    public boolean onMenuItemClick(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.add:
-                Intent intent = new Intent(this, FolderBackupConfigActivity.class);
-                intent.putExtra(FOLDER_BACKUP_REMOTE_PATH, true);
-                startActivity(intent);
-                return true;
-            case R.id.clear_all:
-                mAdapter.notifyDataClear();
-                SettingsManager.instance().saveBackupPaths("");
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/seafile/seadroid2/ui/activity/search/SearchRecyclerViewAdapter.java
+++ app/src/main/java/com/seafile/seadroid2/ui/activity/search/SearchRecyclerViewAdapter.java
@@ -6,7 +6,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
